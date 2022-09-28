eaed56a02e35c1f10ad1783ca1d94b8cdeab1d8e
Vinícius Soares <vinisoares15@gmail.com>
2022-09-28 09:19:27 -0300

    Últimas alterações da documentação

--- ReadFile.js
+++ ReadFile.js
@@ -1,3 +1,25 @@
+
+/**
+ * Autor: Vinicius Soares de Figueiredo - Estagiário de TI - 2022 - Algar Telecom
+ * 
+ * ============================================== Funcionalidades: ===============================================
+ * Este código tem como objetivo varrer/ler um diretório recursivamente e procurar um termo dentro
+ * de todos os arquivos de texto (scripts, .txt, .js, .java, etc) presentes. Caso encontre o termo,
+ * ele executa um comando shell (git blame) para encontrar a data de criação do arquivo, a linha em
+ * que está presente, o autor e o pwd. O resultado é impresso no terminal.
+ * 
+ * ============================================== Pré-requisitos: =================================================
+ * > NodeJS
+ * > Login no Git
+ * > O diretório deve estar versionado no Git (só assim poderá ser usado o comando git blame)
+ * 
+ * ============================================== Como rodar a aplicação: ======================================== 
+ * O arquivo ReadFile.js deverá estar dentro do repositório que deseja fazer a busca.
+ * Alterar os valores das variáveis termo, dataProcurada e filePath de acordo com o que deseja buscar.
+ * Rodar o comando no terminal: node ReadFile.js
+ * Após a execução, o resultado será impresso no terminal.
+ */
+
 const {
     exec
 } = require('child_process');
@@ -9,16 +31,15 @@ const {
 var readline = require('readline');
 
 //Parâmetros de entrada: ano, termo e diretório
-var termo = "ManagedResource" || "ManagedAttribute" || "ApplicationProperty";
-var filePath = "/Users/viniciussoares/Desktop/Algar_Telecom/LeituraDoCodigo/";
-var dataProcurada = '2022'; // caso queira especificar a data, colocar no formato "yyyy-mm-dd"
+var termo = "ManagedResource" || "ManagedAttribute" || "ApplicationProperty" || "AtomicBoolean"; //Termo a ser procurado nas linhas do arquivo
+var filePath = "/Users/"; //especificar o diretório que deseja varrer.
+var dataProcurada = '2020'; // caso queira especificar a data, colocar no formato "yyyy-mm-dd"
 
-var filePath1 = "/Users/viniciussoares/Desktop/Algar_Telecom/algarcrm/algarcrm/source/modules/sales/src/main/java/br/com/algartelecom/algarcrm/sales/service/impl/"
 
 //variavel global blame onde será armazenado o resultado do comando shell
 var blame = [];
-// Para encontrar o termo lendo o arquivo
-function readFileAndFind(file, lineini, linefim) {
+// A função readFileAndFind serve para encontrar o termo lendo o arquivo
+function readFileAndFind(file, lineini, linefim) { 
     var content = fs.createReadStream(file);
     const line_counter = ((i = 0) => () => ++i)();
     const rl = readline.createInterface({
@@ -27,18 +48,19 @@ function readFileAndFind(file, lineini, linefim) {
     rl.on("line", (line, lineno = line_counter()) => {
         //1...2...3...10...100...etc
         if (line.includes(termo)) {
-            //criar uma função assincrona para executar o comando shell
-            exec(`git blame ${file} -L ${lineno},${lineno}`, (err, stdout, stderr) => {
+            //comando shell para encontrar a data de criação do arquivo, a linha em que está presente, o autor e o pwd
+            exec(
+                `cd ${filePath} && git blame -L ${lineno},${lineno} ${file}`, (err, stdout, stderr) => {
                 if (err) {
-                    // node couldn't execute the command
+                    console.log(err);
                     return;
                 }
                 // the *entire* stdout and stderr (buffered)
-                //blame.push(stdout + " " + file);
-                if (stdout.includes(dataProcurada)) {
-                    console.log(stdout + " " + file);
+                if (stdout.includes(dataProcurada)) { //filtro para encontrar a data especificada
+                    console.log("\033[42;1;37m Data procurada: " + `${dataProcurada}` + "\u001b[0m "+ stdout + " \033[44;1;37m " + file + "\u001b[0m ");
+                                  //essas notações de cores são para imprimir no terminal com cores diferentes  
                 } else {
-                    console.log(`Termo encontrado, porém, data diferente de ${dataProcurada}`);
+                    return
                 }
             });
         } else {
@@ -73,48 +95,11 @@ var walk = function (dir, done) {
 };
 
 /**
- * Testando a função com data local e sem filtro (working)
+ *  Execução da função walk
  */
 walk(filePath, function (err, results) {
     if (err) throw err;
-    //console.log(results);
 });
 
-/**
- * Testando a função com data local e sem filtro (working)
- */
-//  walk("/Users/viniciussoares/Desktop/Algar_Telecom/algarcrm/algarcrm/source/modules/phone/", function (err, results) {
-//     if (err) throw err;
-//     //console.log(results);
-// });
 
 
-/**
- * Testando a função com data local e com filtro (working)
- */
-
-// walk("/Users/viniciussoares/Desktop/Algar_Telecom/LeituraDoCodigo/", function (err, results) {
-//     if (err) throw err;
-//  //mostrar apenas termos não nulos e não vazios
-//     var filtered = results.filter(function (item) {
-//     return item !== "";
-//     }).join("\n");
-//     console.log(filtered);
-//     //console.log(results);
-// });
-
-
-/**
- * Testando a função com data do algarcrm e com filtro (working)
- *
- */
-
-// walk("/Users/viniciussoares/Desktop/Algar_Telecom/algarcrm/algarcrm/source/modules/phone/", function (err, results) {
-//     if (err) throw err;
-//     //mostrar apenas termos não nulos e não vazios
-//     var filtered = results.filter(function (item) {
-//         return item !== "";
-//     }).join("\n");
-//     console.log(filtered);
-//     //console.log(results);
-// });
